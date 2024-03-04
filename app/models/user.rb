842c8fda7783cb792df54770358dc9017f1ac745
perldork <perldork@webwizarddesign.com>
2024-03-04 11:39:22 -0500

    basic validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  include ActiveModel::SecurePassword
+  has_many :orders
+  has_secure_password
+  validates :username, presence: true
+  validates :address, presence: true
+  validates :password, presence: false
+  validates :role, inclusion: { in: %w[admin user] }
 end
