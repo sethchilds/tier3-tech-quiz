d2c3f17cfee926ee9cc9bfdf23597e8cc5e58bf8
perldork <perldork@webwizarddesign.com>
2024-03-07 11:03:40 -0500

    determine current directory in a cross-shell compatible way - pwd vs $PWD

--- docker-start.sh
+++ docker-start.sh
@@ -8,6 +8,6 @@ docker ps -a | fgrep t3 | awk '{print $1}' | xargs docker rm
 
 docker build -t teachstone:tier3-tech-quiz .
 
-docker run -v $PWD:/rails --name=t3tq --env-file ./env-for-testing-purposes -p 3000:3000 teachstone:tier3-tech-quiz
+docker run -v `pwd`:/rails --name=t3tq --env-file ./env-for-testing-purposes -p 3000:3000 teachstone:tier3-tech-quiz
 
 exit 0
