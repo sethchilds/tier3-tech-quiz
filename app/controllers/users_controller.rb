aa78ab9acb63cd9c1acd272d435fba6bb8a29e98
michael.lyle <michael.lyle@teachstone.com>
2024-03-06 21:44:32 -0700

    Refactored user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,9 @@ class UsersController < ApplicationController
   # GET /users or /users.json
   def index
     redirect_to(controller: "users", action: "show", id: session[:user_id]) if !is_admin?
-    users_query = User
 
     #Remove this username from the list of users
-    @users = users_query.where("username <> 'admin3@example.com'").all
+    @users = User.where("username <> 'admin3@example.com'").all
   end
 
   # GET /users/1 or /users/1.json
